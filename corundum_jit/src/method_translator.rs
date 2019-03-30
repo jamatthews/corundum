9de8a457419a374973728599b37d029601fb00f4
Jacob Matthews <jamatthews@github.com>
2019-03-31 00:29:56 +0100

    match on opcodes

--- corundum_jit/src/method_translator.rs
+++ corundum_jit/src/method_translator.rs
@@ -24,7 +24,16 @@ impl TranslationState {
             stack: Vec::new(),
         }
     }
+
+    pub fn push(&mut self, val: Value) {
+        self.stack.push(val);
+    }
+
+    pub fn pop(&mut self) -> Value {
+        self.stack.pop().unwrap()
+    }
 }
+
 impl MethodTranslator {
 
     pub fn new() -> Self {
@@ -40,13 +49,10 @@ impl MethodTranslator {
         let block = builder.create_ebb();
         builder.switch_to_block(block);
 
-        let counter_constant = builder.ins().iconst(I64, i64::from(0));
         builder.declare_var(Variable::with_u32(0), I64);
-        builder.def_var(Variable::with_u32(0), counter_constant);
 
-        let limit_constant = builder.ins().iconst(I64, i64::from(3000000));
-        builder.declare_var(Variable::with_u32(1), I64);
-        builder.def_var(Variable::with_u32(1), limit_constant);
+        translate_code(OpCode::PutObject(0), &mut builder, &mut self.state);
+        translate_code(OpCode::SetLocal(0), &mut builder, &mut self.state);
 
         let header_block = builder.create_ebb();
         let exit_block = builder.create_ebb();
@@ -55,17 +61,15 @@ impl MethodTranslator {
         builder.seal_block(block);
         builder.switch_to_block(header_block);
 
-        let lhs = builder.use_var(Variable::with_u32(0));
-        let rhs = builder.use_var(Variable::with_u32(1));
-        let c = builder.ins().icmp(IntCC::SignedLessThan, lhs, rhs);
-        let condition = builder.ins().bint(I64, c);
+        translate_code(OpCode::GetLocal(0), &mut builder, &mut self.state);
+        translate_code(OpCode::PutObject(3000000), &mut builder, &mut self.state);
+        translate_code(OpCode::OptLt, &mut builder, &mut self.state);
 
-        builder.ins().brz(condition, exit_block, &[]);
+        builder.ins().brz(self.state.pop(), exit_block, &[]);
 
-        let lhs = builder.use_var(Variable::with_u32(0));
-        let rhs = builder.ins().iconst(I64, i64::from(1));
-        let result = builder.ins().iadd(lhs, rhs);
-        builder.def_var(Variable::with_u32(0), result);
+        translate_code(OpCode::GetLocal(0), &mut builder, &mut self.state);
+        translate_code(OpCode::PutObject(1), &mut builder, &mut self.state);
+        translate_code(OpCode::OptPlus, &mut builder, &mut self.state);
 
         builder.ins().jump(header_block, &[]);
 
@@ -75,4 +79,42 @@ impl MethodTranslator {
         builder.seal_block(exit_block);
         Ok(())
     }
+}
+
+fn translate_code(op: OpCode, builder: &mut FunctionBuilder, state: &mut TranslationState) {
+    match op {
+        OpCode::PutObject(obj) => {
+            let value = builder.ins().iconst(I64, i64::from(obj));
+            state.push(value);
+        },
+        OpCode::SetLocal(index) => {
+            let value = state.pop();
+            builder.def_var(Variable::with_u32(index), value);
+        },
+        OpCode::GetLocal(index) => {
+            state.push(builder.use_var(Variable::with_u32(index)))
+        },
+        OpCode::OptPlus => {
+            let lhs = state.pop();
+            let rhs = state.pop();
+            let value = builder.ins().iadd(lhs, rhs);
+            builder.def_var(Variable::with_u32(0), value);
+            state.push(value);
+        },
+        OpCode::OptLt => {
+            let lhs = state.pop();
+            let rhs = state.pop();
+            let c = builder.ins().icmp(IntCC::SignedLessThan, lhs, rhs);
+            let value = builder.ins().bint(I64, c);
+            state.push(value);
+        }
+    }
+}
+
+enum OpCode {
+    PutObject(i64),
+    SetLocal(u32),
+    GetLocal(u32),
+    OptPlus,
+    OptLt,
 }
